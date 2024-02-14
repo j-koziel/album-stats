0390c47eaf0e320772f02f494ad8d6f20a506061
j-koziel <jonathankoziel1@gmail.com>
2024-02-14 12:21:51 +0000

    fix: that is wayyy too big

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between text-9xl">
       yo
-      <p>Please first login with your spotify account</p>
-      <p>Then you will be redirected to the albums search page</p>
+      <p className="text-xl">Please first login with your spotify account</p>
+      <p className="text-xl">
+        Then you will be redirected to the albums search page
+      </p>
       <SpotifyAuthButton />
     </main>
   );
