dcb81130aa5f72b9ebf86d696ac4b9ce6aae8fc0
j-koziel <jonathankoziel1@gmail.com>
2024-02-15 10:48:02 +0000

    feat: enable vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+
 import { Inter } from "next/font/google";
 import { Providers } from "../components/providers";
 import { NavMenu } from "../components/nav-menu";
@@ -22,6 +24,7 @@ export default function RootLayout({
         <Providers>
           <NavMenu />
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
